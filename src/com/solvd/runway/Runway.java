4dbbc9826c548f057427e62b2a43939fcd68b639
Unknowny <MyEmail>
2021-06-10 12:03:15 +0300

    Added ArrayList, LinkedList and foreach

--- src/com/solvd/runway/Runway.java
+++ src/com/solvd/runway/Runway.java
@@ -4,9 +4,23 @@ import com.solvd.aircraft.Aircraft;
 import com.solvd.aircraft.civil.Civil;
 import com.solvd.aircraft.military.Military;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class Runway {
 
     private int classNum;
+    private final double airIndex = 1.05;
+    private List<Military> militaryList;
+    private List<Civil> civilList;
+    private List<Aircraft> aircraftList;
+
+    public Runway(){
+        militaryList = new ArrayList<>();
+        civilList = new ArrayList<>();
+        aircraftList = new LinkedList<>();
+    }
 
     public void setClassNum(int classNum){
         this.classNum = classNum;
@@ -16,39 +30,64 @@ public abstract class Runway {
         return this.classNum;
     }
 
-    public void printAccess(Military aircraft){
+    public void addRunwayList(Civil plane){
+        if (printAccess(plane)){
+            civilList.add(plane);
+            addAircraftList(plane);
+        } else {
+            System.out.println(plane.getPlaneName() + " is not suitable for accommodation.");
+        }
+    }
+
+    public void addRunwayList(Military plane){
+        if (printAccess(plane)){
+            militaryList.add(plane);
+            addAircraftList(plane);
+        } else {
+            System.out.println(plane.getPlaneName() + " is not suitable for accommodation.");
+        }
+    }
+
+    private void addAircraftList(Military aircraft){
+        aircraftList.add(aircraft);
+    }
+
+    private void addAircraftList(Civil aircraft){
+        aircraftList.add(aircraft);
+    }
+
+    public List<Aircraft> getAircraftList(){
+        return aircraftList;
+    }
+
+    public boolean printAccess(Military aircraft){
         double airClass = aircraft.getAirClass();
-        if (airClass <= (getClassNum()*1.05)){
-            System.out.println("Landing permit");
-        } else {
-            System.out.println("Denied aircraft landing");
-        }
+        return airClass <= (getClassNum() * airIndex);
     }
 
-    public void printAccess(Civil aircraft){
+    public boolean printAccess(Civil aircraft){
         double airClass = aircraft.getAirClass();
-        if (airClass <= (getClassNum()*1.05)){
-            System.out.println("Landing permit");
-        } else {
-            System.out.println("Denied aircraft landing");
-        }
+        return airClass <= (getClassNum() * airIndex);
     }
 
-    public void printAccess(int aircraftClass){
-        double airClass = aircraftClass;
-        if (airClass <= (getClassNum()*1.05)){
-            System.out.println("Landing permit");
-        } else {
-            System.out.println("Denied aircraft landing");
-        }
-    }
+    public void printRunwayList(){
 
-    public void printAccess(double aircraftClass){
-        double airClass = aircraftClass;
-        if (airClass <= (getClassNum()*1.05)){
-            System.out.println("Landing permit");
-        } else {
-            System.out.println("Denied aircraft landing");
+        if ( civilList.size()>0 ) {
+            System.out.println("\n----------------------------Civil aircraft" +
+                    "----------------------------\n");
+
+            for (Civil aircraft : civilList) {
+                System.out.println(aircraft.getPlaneName());
+            }
+        }
+
+        if ( militaryList.size()>0 ) {
+            System.out.println("\n----------------------------Military aircraft" +
+                    "----------------------------\n");
+
+            for (Military aircraft : militaryList) {
+                System.out.println(aircraft.getPlaneName());
+            }
         }
     }
 

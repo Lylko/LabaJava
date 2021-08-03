8aa5a40666b834e37fb6c8606dccae5f6ba0ec2d
Unknowny <MyEmail>
2021-08-03 18:27:58 +0300

    Some corrections

--- src/main/java/com/solvd/Executor.java
+++ src/main/java/com/solvd/Executor.java
@@ -1,9 +1,5 @@
 package com.solvd;
 
-import com.solvd.aircraft.civil.airliner.Airliner;
-import com.solvd.runway.Airport;
-import com.solvd.utils.DataOperations;
-
 public class Executor {
     public static void main(String[] args){
 

7061b12def3a7035ab97882a362939fe7bd43e8a
Unknowny <MyEmail>
2021-08-05 19:49:42 +0300

    Some corrections

--- src/main/java/com/solvd/database/DataOperations.java
+++ src/main/java/com/solvd/database/DataOperations.java
@@ -14,7 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class DataOperations {
+public abstract class DataOperations {
 
     public static Airport loadRunway(Airport airport, String runwayName){
 
